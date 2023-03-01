c5c991e45b9c0c6349901a155dbdcbeac45a0c82
Alvin Cherk <alvin@cherk.org>
2023-03-01 12:01:32 +1100

    Add dockerfile configurations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { logger } from "@utils/logger";
 import { app } from "./app";
 
-const port = process.env.PORT ?? 5000;
+const port = process.env.PORT ?? 8080;
 
 app.listen(port, () => {
     logger.info("=================================");
