ba6ca67e3a19f3dfefe23a0cf078349807941cb6
Alvin Cherk <alvin@cherk.org>
2023-03-02 15:26:30 +1100

    Add testing buttons on home page for example route

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,12 @@
+import { toast } from "react-toastify";
 import Head from "next/head";
 import { Button } from "@mui/material";
 import { getAuth, signOut } from "firebase/auth";
 import { GetServerSideProps } from "next";
 import { AuthAction, useAuthUser, withAuthUser, withAuthUserTokenSSR } from "next-firebase-auth";
 import { Footer, Header } from "components";
+import { HttpException } from "util/HttpExceptions";
+import { examplePost } from "util/api";
 import initAuth from "util/firebase";
 
 initAuth(); // SSR maybe, i think...
@@ -14,8 +17,49 @@ type HomePageProps = {
 
 const HomePage = ({ email }: HomePageProps): JSX.Element => {
   console.log("On client side,", email);
-
   const authUser = useAuthUser();
+
+  const handleBadRequest = async () => {
+    const [res, err] = await examplePost(
+      "http://localhost:8080/example",
+      (await authUser.getIdToken()) as string,
+      {},
+    );
+
+    if (err !== null) {
+      // Post request did not succeed
+      if (err instanceof HttpException) {
+        toast.error(err.getMessage());
+      } else {
+        // Unknown error
+        toast.error(err); // Probably do better error handling here
+      }
+      return;
+    }
+    if (res === null) throw new Error("Response and error are null"); // Actual error that should never happen
+    toast.success(res.message);
+  };
+
+  const handleGoodRequest = async () => {
+    const [res, err] = await examplePost(
+      "http://localhost:8080/example",
+      (await authUser.getIdToken()) as string,
+      { message: "Hello from frontend!" },
+    );
+
+    if (err !== null) {
+      // Post request did not succeed
+      if (err instanceof HttpException) {
+        toast.error(err.getMessage());
+      } else {
+        // Unknown error
+        toast.error(err); // Probably do better error handling here
+      }
+      return;
+    }
+    if (res === null) throw new Error("Response and error are null"); // Actual error that should never happen
+    toast.success(res.message);
+  };
 
   return (
     <>
@@ -29,7 +73,7 @@ const HomePage = ({ email }: HomePageProps): JSX.Element => {
         <h1 className="text-center pt-4 text-4xl">stuff {email}</h1>
         <div className="flex flex-row justify justify-center py-10">stuff</div>
         <div className="flex flex-col items-center">
-          <div className="w-[100px]">
+          <div className="w-[300px]">
             <Button
               variant="outlined"
               onClick={() => {
@@ -45,6 +89,23 @@ const HomePage = ({ email }: HomePageProps): JSX.Element => {
               }}
             >
               Debug
+            </Button>
+
+            <Button
+              variant="outlined"
+              onClick={() => {
+                handleBadRequest();
+              }}
+            >
+              Send bad example request
+            </Button>
+            <Button
+              variant="outlined"
+              onClick={() => {
+                handleGoodRequest();
+              }}
+            >
+              Send good example request
             </Button>
           </div>
         </div>
--- frontend/util/HttpExceptions.ts
+++ frontend/util/HttpExceptions.ts
@@ -2,12 +2,20 @@
 export class HttpException extends Error {
   public status: number;
   public message: string;
-  public err: any | undefined;
+  public originalError: any | undefined;
 
-  constructor(status: number, message: string, err: any = undefined) {
+  constructor(status: number, message: string, originalError: any = undefined) {
     super(message);
     this.status = status;
     this.message = message;
-    this.err = err;
+    this.originalError = originalError;
+  }
+
+  public getStatusCode(): number {
+    return this.status;
+  }
+
+  public getMessage(): string {
+    return this.message;
   }
 }
