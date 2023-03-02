ac54d39b049ae0c88309aa7a70ca52d54ccc9c27
Alvin Cherk <alvin@cherk.org>
2023-03-02 13:00:33 +1100

    Move firebase admin keys to env

--- backend/src/utils/firebase.ts
+++ backend/src/utils/firebase.ts
@@ -1,13 +1,20 @@
-import { initializeApp } from "firebase-admin/app";
+import { cert, initializeApp } from "firebase-admin/app";
+import validateEnv from "./validateEnv";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyA8ZhV0JWeXho7yZ_y9D201BNsTB_VP-0g",
-    authDomain: "capstone390023t1-githappens.firebaseapp.com",
-    projectId: "capstone390023t1-githappens",
-    storageBucket: "capstone390023t1-githappens.appspot.com",
-    messagingSenderId: "913063051631",
-    appId: "1:913063051631:web:5330462318bbbd2108689a",
-    measurementId: "G-PCLLHRRG4Q",
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA8ZhV0JWeXho7yZ_y9D201BNsTB_VP-0g",
+//     authDomain: "capstone390023t1-githappens.firebaseapp.com",
+//     projectId: "capstone390023t1-githappens",
+//     storageBucket: "capstone390023t1-githappens.appspot.com",
+//     messagingSenderId: "913063051631",
+//     appId: "1:913063051631:web:5330462318bbbd2108689a",
+//     measurementId: "G-PCLLHRRG4Q",
+// };
 
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp({
+    credential: cert({
+        projectId: validateEnv.FIREBASE_PROJECT_ID,
+        privateKey: JSON.parse(validateEnv.FIREBASE_PRIVATE_KEY),
+        clientEmail: validateEnv.FIREBASE_CLIENT_EMAIL,
+    }),
+});
--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -1,11 +1,14 @@
+import { config } from "dotenv";
 import { cleanEnv, port, str } from "envalid";
 
-const validateEnv = () => {
-    console.log(process.env);
-    cleanEnv(process.env, {
-        // NODE_ENV: str(),
-        // PORT: port(),
-    });
-};
+config({ path: ".env" });
+
+const validateEnv = cleanEnv(process.env, {
+    NODE_ENV: str({ default: "development" }),
+    PORT: port({ default: 8080 }),
+    FIREBASE_PRIVATE_KEY: str(),
+    FIREBASE_PROJECT_ID: str(),
+    FIREBASE_CLIENT_EMAIL: str(),
+});
 
 export default validateEnv;
