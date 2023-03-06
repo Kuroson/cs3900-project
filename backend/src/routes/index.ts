66bbe6eba1151bdb8faeb9d126ba92b6928bd88a
Alvin Cherk <alvin@cherk.org>
2023-03-06 21:43:51 +1100

    Add route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { registerController } from "./auth/register.route";
 import { exampleController } from "./example.route";
 import { indexController } from "./index.route";
 
@@ -6,3 +7,4 @@ export const indexRouter = Router();
 
 indexRouter.get("/", indexController);
 indexRouter.all("/example", exampleController);
+indexRouter.all("/auth/register", registerController);
