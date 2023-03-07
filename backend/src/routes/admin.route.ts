9ca4dc3b5847729957d1d2fd0ba572897b38d73e
Alex WSL ALEX-WS <z5204704@ad.unsw.edu.au>
2023-03-07 11:18:46 +1100

    Fixed lint errors

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -10,7 +10,7 @@ type ResponsePayload = {
     message?: string;
 };
 
-type QueryPayload = {};
+type QueryPayload = Record<string, never>;
 
 export const adminController = async (
     req: Request<QueryPayload>,
