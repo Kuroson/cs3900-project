23e1066cedd8a77c09e58ac98314d061abc2facf
Alvin Cherk <alvin@cherk.org>
2023-03-07 19:29:55 +1100

    Add route to app

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { registerController } from "./auth/register.route";
 import { exampleController } from "./example.route";
 import { indexController } from "./index.route";
+import { userDetailsController } from "./user/userDetails.route";
 
 export const indexRouter = Router();
 
 indexRouter.get("/", indexController);
 indexRouter.all("/example", exampleController);
 indexRouter.all("/auth/register", registerController);
+indexRouter.all("/user/details", userDetailsController);
