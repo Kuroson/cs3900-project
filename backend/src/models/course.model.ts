722e47ee30eb20be6dc7903f3fc08b4eb5fe1170
Alex WSL ALEX-WS <z5204704@ad.unsw.edu.au>
2023-03-09 09:24:23 +1100

    Initial implementation of creating course route

--- backend/src/models/course.model.ts
+++ backend/src/models/course.model.ts
@@ -4,18 +4,20 @@ import { User } from "./user.model";
 
 export interface Course extends Document {
     title: string;
+    code: string;
     description?: string;
     session: string;
-    course_icon?: string;
+    icon?: string;
     creator: User["_id"];
     pages?: Types.DocumentArray<Page["_id"]>;
 }
 
 const courseSchema: Schema = new Schema<Course>({
     title: { type: String, required: true },
+    code: { type: String, required: true },
     description: { type: String },
     session: { type: String, required: true },
-    course_icon: { type: String },
+    icon: { type: String },
     creator: { type: Schema.Types.ObjectId, ref: "User", required: true },
     pages: [{ type: Schema.Types.ObjectId, ref: "Page" }],
 });
--- backend/src/routes/course/createCourse.route.ts
+++ backend/src/routes/course/createCourse.route.ts
@@ -1,14 +1,105 @@
 import { HttpException } from "@/exceptions/HttpException";
 import Course from "@/models/course.model";
-import { verifyIdToken } from "@/utils/firebase";
+import User from "@/models/user.model";
+import { verifyIdTokenValid } from "@/utils/firebase";
 import { logger } from "@/utils/logger";
 import { getMissingBodyIDs, isValidBody } from "@/utils/util";
 import { Request, Response } from "express";
 
-export const createCourseController = async (req: Request, res: Response) => {
-    // Sends a post request to create a new Course
-    // May or may not return anything
-    // const course = new Course({
-    // })
-    return res.status(200).json({});
+type ResponsePayload = {
+    courseId: string;
+    message?: string;
+};
+
+type QueryPayload = {
+    code: string;
+    title: string;
+    session: string;
+    description: string;
+    icon: string;
+};
+
+// Sends a post request to create a new Course
+export const createCourseController = async (
+    req: Request<QueryPayload>,
+    res: Response<ResponsePayload>,
+) => {
+    try {
+        if (req.headers.authorization === undefined)
+            throw new HttpException(405, "No authorization header found");
+
+        // Verify token
+        const token = req.headers.authorization.split(" ")[1];
+        const authUser = await verifyIdTokenValid(token);
+
+        // User has been verified
+        if (
+            isValidBody<QueryPayload>(req.body, ["code", "title", "session", "description", "icon"])
+        ) {
+            // Body has been verified
+            const queryBody = req.body;
+
+            const courseId = await createCourse(queryBody, authUser.uid);
+
+            logger.info(`courseId: ${courseId}`);
+            return res.status(200).json({ courseId });
+        } else {
+            throw new HttpException(
+                400,
+                `Missing body keys: ${getMissingBodyIDs<QueryPayload>(req.body, [])}`,
+            );
+        }
+    } catch (error) {
+        if (error instanceof HttpException) {
+            logger.error(error.getMessage());
+            logger.error(error.originalError);
+            return res
+                .status(error.getStatusCode())
+                .json({ message: error.getMessage(), courseId: "" });
+        } else {
+            logger.error(error);
+            return res
+                .status(500)
+                .json({ message: "Internal server error. Error was not caught", courseId: "" });
+        }
+    }
+};
+
+export const createCourse = async (queryBody: QueryPayload, firebase_uid: string) => {
+    const { code, title, session, description, icon } = queryBody;
+
+    const adminId = await User.findOne({ firebase_uid })
+        .then((res) => {
+            if (res === null) {
+                throw new Error("Invalid user in database");
+            }
+            return res._id;
+        })
+        .catch((err) => {
+            throw new Error("Invalid user in database");
+        });
+
+    const myCourse = new Course({
+        title,
+        code,
+        description,
+        session,
+        icon,
+        creator: adminId,
+    });
+
+    const courseId = await myCourse
+        .save()
+        .then((res) => {
+            return res._id;
+        })
+        .catch((err) => {
+            return null;
+        });
+
+    if (courseId === null) {
+        throw new Error("Failed to create course");
+    }
+
+    return courseId;
 };
