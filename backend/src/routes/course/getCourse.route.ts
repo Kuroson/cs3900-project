023b632265f9c4842415dae1fd365dc43ad9a4fb
Alexander <56786655+amcunio@users.noreply.github.com>
2023-03-10 21:31:07 +1100

    Merging course api into master (#19)

---
+++ backend/src/routes/course/getCourse.route.ts
@@ -0,0 +1,66 @@
+import { HttpException } from "@/exceptions/HttpException";
+import Course from "@/models/course.model";
+import { verifyIdTokenValid } from "@/utils/firebase";
+import { logger } from "@/utils/logger";
+import { Request, Response } from "express";
+
+type ResponsePayload = {
+    code?: string;
+    title?: string;
+    description?: string;
+    session?: string;
+    icon?: string;
+    pages?: Array<object>;
+    message?: string;
+};
+
+type QueryPayload = {
+    courseCode: string;
+};
+
+export const getCourseController = async (
+    req: Request<QueryPayload>,
+    res: Response<ResponsePayload>,
+) => {
+    try {
+        if (req.headers.authorization === undefined)
+            throw new HttpException(405, "No authorization header found");
+
+        // Verify token
+        const token = req.headers.authorization.split(" ")[1];
+        const authUser = await verifyIdTokenValid(token);
+
+        // User has been verified
+        // Get course id from url param
+        const ret_data = await getCourse(req.params.courseCode);
+
+        logger.info(ret_data);
+        return res.status(200).json(ret_data);
+    } catch (error) {
+        if (error instanceof HttpException) {
+            logger.error(error.getMessage());
+            logger.error(error.originalError);
+            return res.status(error.getStatusCode()).json({ message: error.getMessage() });
+        } else {
+            logger.error(error);
+            return res.status(500).json({ message: "Internal server error. Error was not caught" });
+        }
+    }
+};
+
+export const getCourse = async (courseId: string) => {
+    const myCourse = await Course.findById(courseId);
+
+    if (myCourse === null) throw new Error("Course does not exist");
+
+    const courseInfo = {
+        code: myCourse.code,
+        title: myCourse.title,
+        description: myCourse.description,
+        session: myCourse.session,
+        icon: myCourse.icon,
+        pages: [], // TODO: when pages are created, update to return pages
+    };
+
+    return courseInfo;
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 import { accessController } from "./admin/access.route";
 import { adminController } from "./admin/admin.route";
 import { registerController } from "./auth/register.route";
+import { createCourseController } from "./course/createCourse.route";
+import { getCourseController } from "./course/getCourse.route";
+import { getCoursesController } from "./course/getCourses.route";
+import { updateCourseController } from "./course/updateCourse.route";
 import { exampleController } from "./example.route";
 import { indexController } from "./index.route";
 import { userDetailsController } from "./user/userDetails.route";
@@ -19,3 +23,9 @@ indexRouter.all("/auth/register", registerController);
 
 // User routes
 indexRouter.all("/user/details", userDetailsController);
+
+// Course routes
+indexRouter.post("/course", createCourseController);
+indexRouter.get("/course", getCoursesController);
+indexRouter.get("/course/:courseCode", getCourseController);
+indexRouter.put("/course/:courseCode", updateCourseController);
