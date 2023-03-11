225f8f1023361ff7ff0d4c50bc10fefcf3f12ff6
Charlene Kwok <charlenekwok499@gmail.com>
2023-03-11 19:01:39 +1100

    GH-5 | get rid of srollbar and change container

--- frontend/components/Layout/ContentContainer.tsx
+++ frontend/components/Layout/ContentContainer.tsx
@@ -7,9 +7,5 @@ type ContentContainerProps = {
 };
 
 export default function ContentContainer({ children }: ContentContainerProps): JSX.Element {
-  return (
-    <Container maxWidth="xl">
-      <Box className={styles.container}>{children}</Box>
-    </Container>
-  );
+  return <div className={styles.container}>{children}</div>;
 }
