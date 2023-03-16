f32851502559db2134afda25cd439a0c4daa44c1
Alvin Cherk <alvin@cherk.org>
2023-03-16 11:24:03 +1100

    Whitespace fixes

--- backend/src/test/bodyParser.spec.ts
+++ backend/src/test/bodyParser.spec.ts
@@ -8,37 +8,31 @@ describe("Test Util Body Parser", () => {
 
     it("Test normal case", () => {
         const body = { name: "John", age: 20 };
-
         expect(isValidBody<Payload>(body, ["name", "age"])).toBe(true);
     });
 
     it("Missing fields", () => {
         const body = { name: "John" };
-
         expect(isValidBody<Payload>(body, ["name", "age"])).toBe(false);
     });
 
     it("No fields", () => {
         const body = {};
-
         expect(isValidBody<Payload>(body, ["name", "age"])).toBe(false);
     });
 
     it("Extra fields", () => {
         const body = { name: "John", age: 20, useless: "stuff" };
-
         expect(isValidBody<Payload>(body, ["name", "age"])).toBe(true);
     });
 
     it("Extra fields and missing fields", () => {
         const body = { name: "John", useless: "stuff" };
-
         expect(isValidBody<Payload>(body, ["name", "age"])).toBe(false);
     });
 
     it("Keys are not strings", () => {
         const body = { name: "John", age: 20, 1: 1 };
-
         expect(isValidBody<Payload>(body, ["name", "age"])).toBe(true);
     });
 });
