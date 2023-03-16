5a9e4e34a936501a961c2427cdbf75fb6bbb0891
Alvin Cherk <alvin@cherk.org>
2023-03-16 11:01:37 +1100

    Change startup logger info print to print localhost link

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,16 @@
 import { logger } from "@utils/logger";
+import validateEnv from "@utils/validateEnv";
 import { app } from "./app";
 
 const port = process.env.PORT ?? 8080;
 
 app.listen(port, () => {
     logger.info("=================================");
-    logger.info(`======= ENV: ${process.env.NODE_ENV} =======`);
+    logger.info(`======= ENV: ${validateEnv.NODE_ENV} =======`);
     logger.info(`🚀 App listening on the port ${port}`);
+    if (validateEnv.NODE_ENV === "development") {
+        logger.info(`== Visit http://localhost:${port} ==`);
+    }
     logger.info("=================================");
 }).on("error", (err) => {
     logger.error(err.message);
