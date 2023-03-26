e8892c8029946d08561d069a5b671296c01abd36
Alex WSL ALEX-WS <z5204704@ad.unsw.edu.au>
2023-03-26 15:38:11 +1100

    Added implementation for delete assignment

---
+++ backend/src/routes/assignment/deleteAssignment.route.ts
@@ -0,0 +1,93 @@
+import { HttpException } from "@/exceptions/HttpException";
+import Assignment from "@/models/course/assignment/assignment.model";
+import Course from "@/models/course/course.model";
+import { checkAuth } from "@/utils/firebase";
+import { logger } from "@/utils/logger";
+import { ErrorResponsePayload, getMissingBodyIDs, isValidBody } from "@/utils/util";
+import { Request, Response } from "express";
+import { checkAdmin } from "../admin/admin.route";
+
+type ResponsePayload = {};
+
+type QueryPayload = {
+    courseId: string;
+    assignmentId: string;
+};
+
+/**
+ * DELETE /assignment/delete
+ * Deletes the given assignment
+ * @param req
+ * @param res
+ * @returns
+ */
+export const deleteAssignmentController = async (
+    req: Request<QueryPayload>,
+    res: Response<ResponsePayload | ErrorResponsePayload>,
+) => {
+    try {
+        const authUser = await checkAuth(req);
+        const KEYS_TO_CHECK: Array<keyof QueryPayload> = ["courseId", "assignmentId"];
+
+        // User has been verified
+        if (isValidBody<QueryPayload>(req.body, KEYS_TO_CHECK)) {
+            // Body has been verified
+            const queryBody = req.body;
+
+            await deleteAssignment(queryBody, authUser.uid);
+
+            return res.status(200).json({});
+        } else {
+            throw new HttpException(
+                400,
+                `Missing body keys: ${getMissingBodyIDs<QueryPayload>(req.body, KEYS_TO_CHECK)}`,
+            );
+        }
+    } catch (error) {
+        if (error instanceof HttpException) {
+            logger.error(error.getMessage());
+            logger.error(error.originalError);
+            return res.status(error.getStatusCode()).json({ message: error.getMessage() });
+        } else {
+            logger.error(error);
+            return res.status(500).json({ message: "Internal server error. Error was not caught" });
+        }
+    }
+};
+
+/**
+ * Deletes the given assignment from the given course
+ *
+ * @param queryBody Arguments containing the fields defined above in QueryPayload
+ * @param firebase_uid Unique identifier of user
+ * @throws { HttpException } Save/recall failed
+ */
+export const deleteAssignment = async (queryBody: QueryPayload, firebase_uid: string) => {
+    if (!(await checkAdmin(firebase_uid))) {
+        throw new HttpException(401, "Must be an admin to create quiz");
+    }
+
+    const { courseId, assignmentId } = queryBody;
+
+    const course = await Course.findById(courseId)
+        .exec()
+        .catch((err) => {
+            logger.error(err);
+            throw new HttpException(500, "Failed to fetch course");
+        });
+    if (course === null) {
+        throw new HttpException(500, "Failed to fetch course");
+    }
+
+    course.assignments.pull(assignmentId);
+
+    await Assignment.findByIdAndDelete(assignmentId).catch((err) => {
+        logger.error(err);
+        throw new HttpException(500, "Failed to delete assignment");
+    });
+
+    await course.save().catch((err) => {
+        logger.error(err);
+        throw new HttpException(500, "Failed to save updated course");
+    });
+};
