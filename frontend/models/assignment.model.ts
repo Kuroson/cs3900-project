20a1fd5dd0c32e67262118d936c6abf3a04997e6
Alex WSL ALEX-WS <z5204704@ad.unsw.edu.au>
2023-04-01 12:04:34 +1100

    Added listing out assignments and add assignment page

---
+++ frontend/models/assignment.model.ts
@@ -0,0 +1,39 @@
+export type AssignmentListType = {
+  assignmentId: string;
+  title: string;
+  description?: boolean;
+  deadline: string;
+};
+
+export type CreateAssignmentType = {
+  courseId: string;
+  title: string;
+  description?: string;
+  deadline: string;
+  marksAvailable: number;
+  tags: Array<string>;
+};
+
+type SubmissionInfo = {
+  title: string;
+  linkToSubmission: string;
+  mark?: number;
+  comments?: string;
+  successTags?: Array<string>;
+  improvementTags?: Array<string>;
+};
+
+export type AssignmentBasicInfo = {
+  title: string;
+  description?: string;
+  deadline: string;
+  marksAvailable: number;
+  tags: Array<string>;
+  submission?: SubmissionInfo; // just for student
+};
+
+export type SubmitAssignmentType = {
+  courseId: string;
+  assignmentId: string;
+  title: string;
+};
---
+++ frontend/pages/instructor/[courseId]/Assignment.tsx
@@ -0,0 +1,188 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import Head from "next/head";
+import AddIcon from "@mui/icons-material/Add";
+import dayjs from "dayjs";
+import { AssignmentListType, CreateAssignmentType } from "models/assignment.model";
+import { UserCourseInformation } from "models/course.model";
+import { CreateQuizType, QuizListType } from "models/quiz.model";
+import { UserDetails } from "models/user.model";
+import { GetServerSideProps } from "next";
+import { AuthAction, useAuthUser, withAuthUser, withAuthUserTokenSSR } from "next-firebase-auth";
+import { AdminNavBar, ContentContainer, Loading } from "components";
+import AddOrEditAssignment from "components/assignment/AddOrEditAssignment";
+import Card from "components/common/Card";
+import PageHeader from "components/common/PageHeader";
+import AddOrEditQuiz from "components/quiz/AddOrEditQuiz";
+import AdminQuiz from "components/quiz/AdminQuiz";
+import { HttpException } from "util/HttpExceptions";
+import { useUser } from "util/UserContext";
+import { getListOfAssignments } from "util/api/assignmentApi";
+import { getUserCourseDetails } from "util/api/courseApi";
+import { createNewQuiz, getListOfQuizzes } from "util/api/quizApi";
+import initAuth from "util/firebase";
+
+initAuth(); // SSR maybe, i think...
+
+type AssignmentProps = {
+  courseData: UserCourseInformation;
+};
+
+const Assignment = ({ courseData }: AssignmentProps): JSX.Element => {
+  // TODO: Fix metadata thing
+  const user = useUser();
+  const authUser = useAuthUser();
+  const [loading, setLoading] = React.useState(user.userDetails === null);
+
+  // Assignment info
+  const [assignmentList, setAssignmentList] = useState<AssignmentListType[]>([]);
+  const [addNewAssignment, setAddNewAssignment] = useState(false);
+  const [openAssignment, setOpenAssignment] = useState(false);
+  const [currentAssignment, setCurrentAssignment] = useState("");
+
+  const handleaddNewAssignment = async (newAssignment: CreateAssignmentType) => {
+    // TODO: backend
+    // const [res, err] = await createNewQuiz(await authUser.getIdToken(), newQuiz, "client");
+    // if (err !== null) {
+    //   console.error(err);
+    //   if (err instanceof HttpException) {
+    //     toast.error(err.message);
+    //   } else {
+    //     toast.error(err);
+    //   }
+    //   return;
+    // }
+    // if (res === null) throw new Error("Response and error are null");
+    // console.log(res);
+    // const quiz = {
+    //   quizId: res.quizId,
+    //   title: newQuiz.title,
+    //   open: newQuiz.open,
+    //   close: newQuiz.close,
+    // };
+    // setQuizList((prev) => [...prev, quiz]);
+    // setAddNewAssignment(false);
+    // toast.success("Quiz created successfully");
+  };
+
+  useEffect(() => {
+    const getAssignments = async () => {
+      const [res, err] = await getListOfAssignments(
+        await authUser.getIdToken(),
+        courseData._id,
+        "client",
+      );
+      if (err !== null) {
+        console.error(err);
+        if (err instanceof HttpException) {
+          toast.error(err.message);
+        } else {
+          toast.error(err);
+        }
+        return;
+      }
+      if (res === null) throw new Error("Response and error are null");
+      setAssignmentList(res.assignments);
+    };
+    getAssignments();
+  }, [authUser, courseData._id]);
+
+  useEffect(() => {
+    // Build user data for user context
+    if (user.userDetails !== null) {
+      setLoading(false);
+    }
+  }, [user.userDetails]);
+
+  if (loading || user.userDetails === null) return <Loading />;
+  const userDetails = user.userDetails as UserDetails;
+
+  return (
+    <>
+      <Head>
+        <title>Assignment</title>
+        <meta name="description" content="Assignment" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <AdminNavBar userDetails={userDetails} courseData={courseData} showAddPage={true} />
+      <ContentContainer>
+        <div className="flex flex-col w-full px-[5%] py-2">
+          {!openAssignment && !addNewAssignment && (
+            <>
+              <PageHeader title="Assignment" />
+              <div className="flex flex-wrap mt-10">
+                {assignmentList.map((assignment) => (
+                  <Card
+                    text={assignment.title}
+                    close={assignment.deadline}
+                    key={assignment.assignmentId}
+                    isQuiz={false}
+                    handleOpen={() => {
+                      setOpenAssignment((prev) => !prev);
+                      setCurrentAssignment(assignment.assignmentId);
+                    }}
+                  />
+                ))}
+                <div
+                  className="flex flex-col rounded-lg items-center justify-center gap-2 shadow-md p-5 my-2 mx-5 w-[350px] h-[160px] cursor-pointer hover:shadow-lg"
+                  onClick={() => setAddNewAssignment((prev) => !prev)}
+                >
+                  <AddIcon color="primary" fontSize="large" />
+                </div>
+              </div>
+            </>
+          )}
+          {addNewAssignment && (
+            <AddOrEditAssignment
+              handleAddNewAssignment={handleaddNewAssignment}
+              closeAssignment={() => setAddNewAssignment((prev) => !prev)}
+              courseId={courseData._id}
+              courseTags={courseData.tags}
+              isEditing={false}
+            />
+          )}
+          {/* {openAssignment && (
+            <AdminQuiz
+              quizId={currentAssignment}
+              handleClose={() => setOpenAssignment((prev) => !prev)}
+              courseId={courseData._id}
+              courseTags={courseData.tags}
+            />
+          )} */}
+        </div>
+      </ContentContainer>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<AssignmentProps> = withAuthUserTokenSSR({
+  whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
+})(async ({ AuthUser, query }): Promise<{ props: AssignmentProps } | { notFound: true }> => {
+  const { courseId } = query;
+
+  if (courseId === undefined || typeof courseId !== "string") {
+    return { notFound: true };
+  }
+
+  const [courseDetails, courseDetailsErr] = await getUserCourseDetails(
+    await AuthUser.getIdToken(),
+    courseId as string,
+    "ssr",
+  );
+
+  if (courseDetailsErr !== null) {
+    console.error(courseDetailsErr);
+    // handle error
+    return { notFound: true };
+  }
+
+  if (courseDetails === null) throw new Error("This shouldn't have happened");
+
+  return { props: { courseData: courseDetails } };
+});
+
+export default withAuthUser<AssignmentProps>({
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(Assignment);
